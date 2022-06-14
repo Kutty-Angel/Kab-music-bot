07385d31fda378cd18b0d77f4396a6f5e15bc532
Kutty-Angel <107125414+Kutty-Angel@users.noreply.github.com>
2022-06-14 04:49:49 -0700

    Create start.py

---
+++ kab/modules/start.py
@@ -0,0 +1,56 @@
+import asyncio
+
+from pyrogram import Client, filters, __version__ as pyrover
+from pyrogram.errors import FloodWait, UserNotParticipant
+from pytgcalls import (__version__ as pytover)
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, ChatJoinRequest
+from kab.utils.filters import command
+
+from kab.config import BOT_USERNAME
+from kab.config import START_PIC
+from kab.config import BOT_NAME
+
+
+
+@Client.on_message(command("start") & filters.private & ~filters.edited)
+async def start_(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"{START_PIC}",
+        caption=f"""Hello 👋 Im **{BOT_NAME}**
+I'm Kutty Angel Music Bot For Playing High Quality and Stable Music in your Groups Voice Chat with Some Useful Features.
+Use inline buttons given below to know more about me !!""",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🏳‍🌈 About", callback_data="cbabout"),
+                    InlineKeyboardButton(
+                        "☁️ Others", callback_data="others")
+                ],
+                [
+                    InlineKeyboardButton(
+                        "🗂 Commands", callback_data="cbevery")
+                ],
+                [
+                    InlineKeyboardButton(
+                        "✚ Add Me to your Group ✚", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+                ]
+           ]
+        ),
+    )
+
+
+@Client.on_message(command(["start", f"start@{BOT_USERNAME}"]) & filters.group & ~filters.edited)
+async def gcstart(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"{START_PIC}",
+        caption=f"Thanks for Kutty Angel in your group !! If you want to use me with right actions promote me as admin in this Chat.",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🤖 Bot Owner", url=f"https://t.me/{OWNER_USERNAME}")
+                ]
+            ]
+        ),
+    )
